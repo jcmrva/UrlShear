79fe521b77d79896155cb452cad32b065660b1be
Josh M <20140997+jcmrva@users.noreply.github.com>
2018-06-11 23:06:46 -0400

    string ops

--- src/UrlShear/Library.fs
+++ src/UrlShear/Library.fs
@@ -2,8 +2,24 @@ namespace UrlShear
 
 open System
 
+[<AutoOpen>]
 module Create =
 
-    let short (original:String) =
+    let short (original:string) =
         original |> ignore
         "meep"
+
+    let isHttp (s:string) =
+        let ignoreCase = StringComparison.InvariantCultureIgnoreCase
+        s.StartsWith("http:", ignoreCase) || s.StartsWith("https:", ignoreCase)
+
+    let ssIdx (s:string) = s.IndexOf("//")
+
+    let firstSlashIdx (s:string) = s.IndexOf("/", (ssIdx s) + 2)
+
+    let hostName (s:string) = 
+        let ss = (ssIdx s) + 2
+        s.Substring(ss, (firstSlashIdx s) - ss)
+
+    let path (s:string) =
+        s.Substring((firstSlashIdx s) + 1)
