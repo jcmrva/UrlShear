9fd45306800834881a9525e0596fbde9789f82ba
Josh M <20140997+jcmrva@users.noreply.github.com>
2018-09-29 13:33:20 -0400

    expecto compile fix

--- src/UrlShear.Tests/Main.fs
+++ src/UrlShear.Tests/Main.fs
@@ -1,6 +1,36 @@
 ﻿module UrlShear.Tests
 open Expecto
 
+module Tests = 
+
+    open UrlShear
+
+    [<Tests>]
+    let internals =
+      testList "internals" [
+        testCase "query string ? is removed" <| fun _ ->
+          let query = removePrefixChar "?param=value" "?"      
+          Expect.equal "param=value" query "leading ? removed"
+        
+        testCase "query string list to string tuple pair" <| fun _ -> 
+          let qparams = "param1=val&param2&&" |> queryToList
+          let expected = [
+            "param1", "val"
+            "param2", ""
+          ]
+          Expect.equal expected qparams "query string parsed"
+        
+      ]
+
+    [<Tests>]
+    let tests =
+      testList "samples" [
+        test "I am (shouldn't fail)" {
+          "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true true
+        }
+      ]
+
+
 [<EntryPoint>]
 let main argv =
     Tests.runTestsInAssembly defaultConfig argv
